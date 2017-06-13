f7e90dc82a1ed012b1bab00ec8b0be2fefc18f62
Sandro Colombo <sandro.colombo.sc@gmail.com>
2017-06-13 23:47:19 +0200

    minor style changes

--- src/index.php
+++ src/index.php
@@ -45,7 +45,6 @@
 
         <section>
             <h1>Bilder Hansruedi Modul 152</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </section>
 
         <section>
