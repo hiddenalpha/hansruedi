564eaa898b89b5e4f38be312f92984513a6fef33
Sandro Colombo <sandro.colombo.sc@gmail.com>
2017-06-13 10:18:15 +0200

    Minor style updates

--- src/client/large-image/large-image.js
+++ src/client/large-image/large-image.js
@@ -1,64 +1,67 @@
-photobook.await( 'largeImage',
-    /*njct*/['allContent'],
-    function( allContent ){
+photobook.await('largeImage',
+    /*njct*/
+    ['allContent'],
+    function (allContent) {
 
 
-        var view = (function(){
-            return $( ''
-                +'<div class="largeImage" style="display:none; transition: 1s ease-in-out;">'
-                +'  <div class="overlay">'
-                +'    <div>'
-                +'      <img x-name="image">'
-                +'    </div>'
-                +'  </div>'
-                +'</div>'
+        var view = (function () {
+            return $('' +
+                '<div class="largeImage" style="display:none; transition: 1s ease-in-out;">' +
+                '  <div class="overlay">' +
+                '    <div>' +
+                '      <img x-name="image">' +
+                '    </div>' +
+                '  </div>' +
+                '</div>'
             );
         }());
 
-        var overlay = view.find( ".overlay" );
-        overlay.on( 'click' , function(){
-            largeImage.setVisible( false );
+        var overlay = view.find(".overlay");
+        overlay.on('click', function () {
+            largeImage.setVisible(false);
         });
-        var img = view.find( "[x-name='image']" );
+        var img = view.find("[x-name='image']");
 
 
         var largeImage, that;
         largeImage = that = {
 
-            setVisible: function( makeVisible ){
-                if( makeVisible === undefined ) makeVisible = true;
-                if( !isVisible() && makeVisible ){
-                    var originWidth = view.css( "width" );
-                    var originHeight = view.css( "height" );
-                    setVisible( true );
-                }else if( isVisible() && !makeVisible ){
-                    setVisible( false );
+            setVisible: function (makeVisible) {
+                if (makeVisible === undefined) makeVisible = true;
+                if (!isVisible() && makeVisible) {
+                    var originWidth = view.css("width");
+                    var originHeight = view.css("height");
+                    document.body.style.overflow = "hidden"
+                    setVisible(true);
+                } else if (isVisible() && !makeVisible) {
+                    document.body.style.overflow = "initial"
+                    setVisible(false);
                 }
             },
 
-            showImage: function( image ){
-                if( !image || !image.src ) throw Error( "IllegalArgument" );
+            showImage: function (image) {
+                if (!image || !image.src) throw Error("IllegalArgument");
                 img[0].src = image.src;
                 that.setVisible();
             }
 
         };
 
-        function isVisible(){
-            return view.css( "display" ) == "block";
+        function isVisible() {
+            return view.css("display") == "block";
         }
 
-        function setVisible( visible ){
-            if( visible === undefined ) visible = true;
+        function setVisible(visible) {
+            if (visible === undefined) visible = true;
             // TODO: Work with animation
-            view.css( "display" , visible?"block":"none" );
+            view.css("display", visible ? "block" : "none");
         }
 
 
         // Append view to DOM.
-        allContent[0].replaceChild( view[0] , allContent.find("#largeImagePlaceholder")[0] );
+        allContent[0].replaceChild(view[0], allContent.find("#largeImagePlaceholder")[0]);
 
         // Do our exports
-        this.resolve( largeImage );
+        this.resolve(largeImage);
     }
 );
